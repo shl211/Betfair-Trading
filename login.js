283e6bd22c14e62276a708f580a00c0d121f6a06
Stephen Liu <shl21@ic.ac.uk>
2024-08-14 13:50:28 +0100

    Add redirect to dashboard page

--- login.js
+++ login.js
@@ -7,16 +7,43 @@ btn.addEventListener("click", () => {
     const password = document.querySelector("#password");
     const APIkey = document.querySelector("#key");
     
-    validateForm(username.value, password.value, APIkey.value);
-
+    validated = validateForm(username.value, password.value, APIkey.value);
+    
+    if(validated) {
+        directToDashboard();
+    }
 });
 
 function validateForm(username,password,key) {
 
-    if(username == "") alert("Please enter your username");
-    else if(password == "") alert("Please enter your password");
-    else if(key == "") alert("Please enter your API key");
-    
+    validInput = checkInput(username,password,key);
+
+    if(!validInput) {
+        return false;
+    }
+
+    return true;
 }
 
+function directToDashboard() {
+    const url = "/dashboard.html";
+    window.location.replace(url);
+}
+
+function checkInput(username,password,key) {
+    if(username == "") {
+        alert("Please enter your username");
+        return false;
+    }
+    else if(password == "") {
+        alert("Please enter your password");
+        return false;
+    }
+    else if(key == "") {
+        alert("Please enter your API key");
+        return false;
+    }
+
+    return true;
+}
 
